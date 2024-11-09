aab29a943c814de9fd59572f7545a7c8ad0b57fe
Dakota Lannen <97769932+GamesBrewing@users.noreply.github.com>
2024-11-08 23:25:00 -0600

    Update integrate.c
    Visual studios messed up and tried to create a new repository for this file, so I am just making the changes manually.

--- Integrate/integrate.c
+++ Integrate/integrate.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <omp.h>
 
 #define N 1000000000 // intervals
+
 
 double f(double x) {
     return 4.0 / (1.0 + x * x); // Function to integrate
@@ -9,6 +11,10 @@ double f(double x) {
 double trapezoidalRule() {
     double h = 1.0 / N; 
     double sum = 0.5 * (f(0) + f(1)); 
+
+    //Sum all threads at the end
+    #pragma omp parallel for reduction(+:sum)
+
     for (int i = 1; i < N; ++i) {
         sum += f(i * h);
     }
@@ -17,6 +23,15 @@ double trapezoidalRule() {
 }
 
 int main() {
+
+    // Set the number of threads
+  //omp_set_num_threads(1);
+  //omp_set_num_threads(2);
+  //omp_set_num_threads(5);
+  //omp_set_num_threads(7);
+  omp_set_num_threads(9);
+
+
     double pi = trapezoidalRule();
     printf("Estimated value of π: %f\n", pi);
     return 0;
